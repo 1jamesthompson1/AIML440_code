5453c403f44bcbdf86eb0e96d65705c675226ff4
James <1jamesthompson1@gmail.com>
2025-03-25 18:28:32 +1300

    Final code before running experiments

--- basic_algo_baseline/continuous.sh
+++ basic_algo_baseline/continuous.sh
@@ -8,17 +8,24 @@ echo "==Listing Files=="
 ls
 
 echo "==Understanding which algorithm to run=="
+task_id=$(($1-1)) # 0-indexed except that grid system doesnt allow 0 as the task id
+num_seeds=10
+num_algos=3
+num_envs=9
 
-seed=$((${1} % 10))
-algo_index=$(( (${1} / 10) % 10))
+runs_per_algo=$(($num_seeds * $num_envs))
+
+seed=$(($task_id % $num_seeds))
+algo_index=$(( ($task_id / $runs_per_algo) % $num_algos))
+env_index=$(( (($task_id % $runs_per_algo) / $num_seeds) % $num_seeds))
 
 algorithms=("ppo" "sac" "td3")
 selected_algo=${algorithms[$algo_index]}
 
-echo " Running ${selected_algo} with seed ${seed}"
+echo " Running ${selected_algo} in env ${env_index} with seed ${seed}"
 
 echo "==Running Script=="
 
-poetry run python train.py --seed=${seed} --output_dir=$OUTPUTDIR --algo=${selected_algo}
+poetry run python train.py --seed=${seed} --output_dir=$OUTPUTDIR --algo=${selected_algo} --env_index=${env_index}
 
 echo "==Script Complete=="
--- basic_algo_baseline/train.py
+++ basic_algo_baseline/train.py
@@ -15,13 +15,17 @@ parser = argparse.ArgumentParser(description='Train a model')
 parser.add_argument('--seed', required=True, type=int, help='Random seed for reproducibility')
 parser.add_argument('--output_dir', type=str, required=True, help='Output directory for the model')
 parser.add_argument('--algo', required=True, type=str, choices=['sac', 'ppo', 'td3'] ,help='Algorithm to use for training')
+parser.add_argument('--env_index', required=True, type=int, help='Environment to train on')
+
 args = parser.parse_args()
 
 os.chdir(args.output_dir)
 
+
 def learn_and_save(model, name, eval_env, **kwargs):
+    print(f"Training {name} inside {os.getcwd()}...")
     eval_env = Monitor(eval_env)
-    eval_callback = EvalCallback(eval_env, eval_freq=1000, n_eval_episodes=5, log_path=None, best_model_save_path=None)
+    eval_callback = EvalCallback(eval_env, eval_freq=1000, n_eval_episodes=10, log_path=None, best_model_save_path=None)
 
     # Manually set the logpath because I want to name the output file
     eval_callback.log_path = f"training_evaluations/{name}.npz"
@@ -34,10 +38,10 @@ time_steps = 1_000_000
 
 tensorboard_logs = "tensorboard_logs"
 
-# Using the hyperparameters from the original papers
+# Using the hyperparameters from the original papers to get as close as possible
 algorithms = {
     "sac": partial(PPO, "MlpPolicy", learning_rate=0.0003, n_steps=2048, batch_size=64, n_epochs=10, gamma=0.99, gae_lambda=0.95, verbose=0, tensorboard_log = tensorboard_logs),
-    "ppo": partial(SAC, "MlpPolicy", learning_rate=0.0003, buffer_size=1000000, learning_starts=100, batch_size=256, tau=0.005, gamma=0.99, train_freq=1, gradient_steps=1, verbose=0, tensorboard_log = tensorboard_logs),
+    "ppo": partial(SAC, "MlpPolicy", learning_rate=0.0003, buffer_size=1e6, learning_starts=100, batch_size=256, tau=0.005, gamma=0.99, train_freq=1, gradient_steps=1, verbose=0, tensorboard_log = tensorboard_logs),
     "td3": partial(TD3, "MlpPolicy", learning_rate=0.001, buffer_size=1000000, learning_starts=1000, batch_size=100, tau=0.005, gamma=0.99, train_freq=1, policy_delay=2, target_policy_noise=0.2, target_noise_clip=0.5, gradient_steps=1, verbose=0, tensorboard_log = tensorboard_logs),
 }
 
@@ -53,17 +57,14 @@ envs = [
     "Pusher-v5",
 ]
 
-continuous_tests = {
-    f"{args.algo}_{env}_{time_steps}_{args.seed}": (algorithms[args.algo](env=gym.make(env), seed = args.seed), env)
-    for env in 
-    envs
-}
-
 os.makedirs("models", exist_ok=True)
 os.makedirs("logs/avg_reward", exist_ok=True)
 
+env = envs[args.env_index]
 
-with ThreadPoolExecutor() as executor:
-    futures = [executor.submit(learn_and_save, model, name, gym.make(env), total_timesteps=time_steps) for name, (model, env) in continuous_tests.items()]
-    for future in tqdm(futures):
-        future.result()
+learn_and_save(
+    algorithms[args.algo](gym.make(env), seed=args.seed),
+    f"{args.algo}_{env}_{time_steps}_{args.seed}",
+    gym.make(env),
+    total_timesteps=time_steps,
+)
